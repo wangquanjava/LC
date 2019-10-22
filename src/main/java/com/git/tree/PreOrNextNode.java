0d249fb9535044beb3285b647a69b4097cdc7a56
wangquan07 <wangquan07@meituan.com>
2019-10-22 16:46:26 +0800

    寻找前驱节点

--- src/main/java/com/git/tree/PreOrNextNode.java
+++ src/main/java/com/git/tree/PreOrNextNode.java
@@ -6,35 +6,73 @@ import com.google.common.collect.Lists;
 import java.util.List;
 
 /**
+ * 获取二叉树任意节点的前驱或后继节点
+ *
  * @author wangquan07
  * 2019/10/22 15:05
  */
 public class PreOrNextNode {
     public static void main(String[] args) {
         List<Node> nodeList = init();
+        Node node = nodeList.get(0);
+        System.out.println("锁定" + node);
 
-        Node preNode = getPreNode(nodeList.get(6));
-        System.out.println(preNode);
+        Node preNode = getPreNode(node);
+        System.out.println("前驱" + preNode);
+
+        Node nextNode = getNextNode(node);
+        System.out.println("后继" + nextNode);
+    }
+
+    private static Node getNextNode(Node node) {
+        if (node.getRightChild() != null) {
+            return getMin(node.getRightChild());
+        } else {
+            return getGreater(node);
+        }
+    }
+
+    private static Node getGreater(Node node) {
+        if (node.getParent() == null) {
+            return null;
+        }
+        if (node.getParent().getLeftChild() == node) {
+            return node.getParent();
+        } else {
+            return getGreater(node.getParent());
+        }
+    }
+
+    private static Node getMin(Node node) {
+        if (node.getLeftChild() != null) {
+            return getMin(node.getLeftChild());
+        } else {
+            return node;
+        }
     }
 
     private static Node getPreNode(Node node) {
         if (node.getLeftChild() != null) {
             return getMax(node.getLeftChild());
         } else {
-            return getRight(node);
+            return getSmaller(node);
         }
     }
 
     /**
      * 获取右斜线(\)
      */
-    private static Node getRight(Node node) {
+    private static Node getSmaller(Node node) {
+        if (node.getParent() == null) {
+            return null;
+        }
+
         if (node.getParent().getRightChild() == node) {
             // 如果父节点的右孩子是自己, 那就返回父节点
             return node.getParent();
         } else {
             // 递归查找父节点
-            return getRight(node.getParent());
+            return getSmaller(node.getParent());
         }
     }
 
